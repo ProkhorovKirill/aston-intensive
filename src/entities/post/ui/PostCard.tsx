9308d0bf8fa90c3dcf092b661c417c17755cb5ca
Kirill <fantazer187@gmail.com>
2025-12-05 11:57:44 +0300

    добавление стилей для ссылок

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,16 +1,15 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import type { Post } from "../model/interfaces";
 import styles from './postCard.module.css'
+import sharedStyles from '../../../shared/ui/shared.module.css'
 
 export default function PostCard({postInfo} : {postInfo: Post}) {
-
-    // const location = useLocation()
 
     return (
         <div className={styles.postCard}>
             <h4 className={styles.postCardTitle}>Заголовок: {postInfo.title}</h4>
             <p className={styles.postCardBody}>{postInfo.body}</p>
-            {location.pathname === '/posts' && <Link to={String(postInfo.id)}>Перейти к посту</Link>}
+            {location.pathname === '/posts' && <Link to={String(postInfo.id)} className={sharedStyles.Link}>Перейти к посту</Link>}
         </div>
     )
 
--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useGetPostQuery } from "../../widgets/PostList/api/postsListApi";
 import PostCard from "../../entities/post/ui/PostCard";
+import sharedStyles from '../../shared/ui/shared.module.css'
 import styles from './postPage.module.css'
 
 export default function PostPage() {
@@ -12,14 +13,19 @@ export default function PostPage() {
     return (
         <>
 
-            <h2>postpage</h2>
+            <h2 className={sharedStyles.centralTitle}>Пост #{urlParams.id}</h2>
 
-            {error && <h2 className={styles.centralTitle}>Произошла ошибка при загрузке поста</h2>}
+            {error && <h2 className={sharedStyles.centralTitle}>Произошла ошибка при загрузке поста</h2>}
 
-            {isLoading && <h2 className={styles.centralTitle}>Идет загрузка! Пожалуйста, подождите!</h2>}
+            {isLoading && <h2 className={sharedStyles.centralTitle}>Идет загрузка! Пожалуйста, подождите!</h2>}
 
             {!isLoading && !error && <PostCard postInfo={post}/>}
+
+            <div className={styles.centralLink}>
+                <Link to='/posts' className={sharedStyles.Link}>Назад</Link>
+            </div>
             
+
         </>
     )
 
