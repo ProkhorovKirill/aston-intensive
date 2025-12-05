8a14cd23daad815325ccca3051aee8a523d3827e
Kirill <fantazer187@gmail.com>
2025-12-05 12:15:43 +0300

    добавление стилей для UserTabs

--- src/widgets/UserTabs/ui/UserTabs.tsx
+++ src/widgets/UserTabs/ui/UserTabs.tsx
@@ -1,19 +1,26 @@
 import { NavLink } from "react-router-dom";
 import { links, type LinkType } from "../lib/links";
+import sharedStyles from '../../../shared/ui/shared.module.css';
+import styles from './userTabs.module.css'
 
 export default function UserTabs() {
 
     return (
 
         <>
-            <h2>Usertabs</h2>
-            <div>
+            <nav className={styles.userTabsContainer}>
                 {links.map((item: LinkType) => {
                     return <div key={item.id}>
-                                <NavLink to={item.to}>{item.text}</NavLink>
+                                <NavLink 
+                                    to={item.to} 
+                                    className={
+                                        ({isActive}) => (isActive ? `${sharedStyles.activeLink}` : `${sharedStyles.Link}`)
+                                    }>
+                                        {item.text}
+                                </NavLink>
                             </div>
                 })}
-            </div>
+            </nav>
         </>
 
     )
