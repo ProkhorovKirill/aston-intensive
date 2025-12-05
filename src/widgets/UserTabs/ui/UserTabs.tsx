9a2ec42fbc4583477891368dba85471eab5f86f1
Kirill <fantazer187@gmail.com>
2025-12-05 13:59:40 +0300

    оптимизация UserTabs с useMemo и useCallback

--- src/widgets/UserTabs/ui/UserTabs.tsx
+++ src/widgets/UserTabs/ui/UserTabs.tsx
@@ -2,24 +2,30 @@ import { NavLink } from "react-router-dom";
 import { links, type LinkType } from "../lib/links";
 import sharedStyles from '../../../shared/ui/shared.module.css';
 import styles from './userTabs.module.css'
+import { useCallback, useMemo } from "react";
 
 export default function UserTabs() {
+
+    const getNavLinkClassName = useCallback(
+        ({ isActive }: { isActive: boolean }): string => 
+            isActive ? `${sharedStyles.activeLink}` : `${sharedStyles.Link}`, []);
+
+    const renderedLinks = useMemo(() => 
+        links.map((item: LinkType) => (
+            <div key={item.id}>
+                <NavLink 
+                    to={item.to} 
+                    className={getNavLinkClassName}>
+                    {item.text}
+                </NavLink>
+            </div>
+        )), [links, getNavLinkClassName]);
 
     return (
 
         <>
             <nav className={styles.userTabsContainer}>
-                {links.map((item: LinkType) => {
-                    return <div key={item.id}>
-                                <NavLink 
-                                    to={item.to} 
-                                    className={
-                                        ({isActive}) => (isActive ? `${sharedStyles.activeLink}` : `${sharedStyles.Link}`)
-                                    }>
-                                        {item.text}
-                                </NavLink>
-                            </div>
-                })}
+                {renderedLinks}
             </nav>
         </>
 
